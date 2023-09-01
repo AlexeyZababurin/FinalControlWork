74cadb2ecabb2938c5309cccbd6049b7528d8f6b
Alexey <Lehan347@yandex.ru>
2023-09-01 16:33:15 +0300

    Wrote a program for the diagram

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿string[] inputArray = {"hello", "2", "world", ":-)", "1234", "15"};
+
+string PrintArray(string[] inputArray)
+{
+    return string.Join(", ", inputArray);
+}
+
+int CountStringInArray(string[] input)
+{
+    int length = inputArray.Length;
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < length; i++)
+    {
+        if (inputArray[i].Length <= maxLengthString)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] CreateNewArray(string[] inputArray, int countWords)
+{
+    int length = inputArray.Length;
+    string[] outputArray = new string[countWords];
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < length; i++)
+    {
+        if (inputArray[i].Length <= maxLengthString)
+        {
+            outputArray[count] = inputArray[i];
+            count++;
+        }
+    }
+    return outputArray;
+}
+
+PrintArray(inputArray);
+string[] outputArray = CreateNewArray(inputArray, CountStringInArray(inputArray));
+PrintArray(outputArray);
